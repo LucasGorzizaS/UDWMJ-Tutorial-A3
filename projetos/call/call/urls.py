e1f1b818d442292582f192a96e6f1169995cc39a
LucasGS <lucas.gorziza@gmail.com>
2023-11-23 20:56:45 -0300

    tutorial 4 finalizado

--- projetos/call/call/urls.py
+++ projetos/call/call/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('categorias/', include('categories.urls', namespace='categories')),
+    path('produtos/', include('products.urls', namespace='products')),
 ]
